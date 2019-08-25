9a058633b3c60a09d1886711b6d5deae98095b42
Alexis <entulinea.alexis@gmail.com>
2019-08-25 22:01:45 +0200

    Improve pipe Sinfoto. Get image without args

--- src/app/pipes/sinfoto.pipe.ts
+++ src/app/pipes/sinfoto.pipe.ts
@@ -5,14 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SinfotoPipe implements PipeTransform {
 
-  transform(pelicula: any, args: any): any {
+  transform(pelicula: any, args?: any): any {
 
     let url = "http://image.tmdb.org/t/p/w500";
-    if( pelicula && pelicula.backdrop_path && (args === "bp") ){
+    let argNoExists = (typeof args === "undefined");
+    if( pelicula && pelicula.backdrop_path && (argNoExists || args === "bp") ){
       return url + pelicula.backdrop_path
     }
     else{
-      if(pelicula && pelicula.poster_path && (args === "pp")){
+      if(pelicula && pelicula.poster_path && (argNoExists || args === "pp")){
         return url + pelicula.poster_path
       }else{
         return "assets/img/noimage.png";
